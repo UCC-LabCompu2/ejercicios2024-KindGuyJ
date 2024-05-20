c9f12fb6ae0fceb55880047bf015e57fcf081d45
KindGuyJ <163864272+KindGuyJ@users.noreply.github.com>
2024-05-20 08:38:00 -0300

    Refactored misFunciones.js

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -4,41 +4,46 @@
  * @param {string} ID
  * @param {number} N
  */
-function convunidades(ID, N) {
+convunidades = (ID, N) => {
+    let metros, pulgadas, yardas, pies
     if (isNaN(N)) {
         alert('El valor ingresado no es un número');
         document.getElementById(ID).value = "";
-        document.getElementById("metro").value = "";
-        document.getElementById("pulgada").value = "";
-        document.getElementById("pie").value = "";
-        document.getElementById("yarda").value = "";
+        metros = "";
+        pulgadas = "";
+        pies = "";
+        yardas = "";
     } else if (ID == "metro") {
-        document.getElementById("pulgada").value = N * 39.3701;
-        document.getElementById("pie").value = N * 3.28084;
-        document.getElementById("yarda").value = N * 1.09361;
+        pulgadas = N * 39.3701;
+        pies = N * 3.28084;
+        yardas = N * 1.09361;
     } else if (ID == "pie") {
-        document.getElementById("pulgada").value = N * 12;
-        document.getElementById("metro").value = N * 0.3048;
-        document.getElementById("yarda").value = N * 0.333333;
+        pulgadas = N * 12;
+        metros = N * 0.3048;
+        yardas = N * 0.333333;
     } else if (ID == "pulgada") {
-        document.getElementById("metro").value = N * 0.0254;
-        document.getElementById("pie").value = N * 0.0833333;
-        document.getElementById("yarda").value = N * 0.0277778;
+        metros = N * 0.0254;
+        pies = N * 0.0833333;
+        yardas = N * 0.0277778;
     } else if (ID == "yarda") {
-        document.getElementById("pulgada").value = N * 36;
-        document.getElementById("pie").value = N * 3;
-        document.getElementById("metro").value = N * 0.9144;
+        pulgadas = N * 36;
+        pies = N * 3;
+        metros = N * 0.9144;
     }
+    document.getElementById("metro").value = metros;
+    document.getElementById("pulgada").value = pulgadas;
+    document.getElementById("pie").value = pies;
+    document.getElementById("yarda").value = yardas;
 }
 
 function convgr(ID){
     if(ID==grados){}
     let grad = document.getElementById(element "grados").value;
-    let rad = grad*Math.PI/180
-    let document.getElementById(element "radianes").value = rad
+    let rad = grad*Math.PI/180;
+    let document.getElementById(element "radianes").value = rad;
     else {if(ID==radianes){
         let rad = document.getElementById(element "radianes").value;
-        let grad = grad*Math.PI/180
-        let document.getElementById(element "grados").value = rad 
+        let grad = grad*Math.PI/180;
+        let document.getElementById(element "grados").value = rad;
     }}
 }
