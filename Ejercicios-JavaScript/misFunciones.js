42b5624cd3f2b03f7c1a471ab503c400cb71858d
KindGuyJ <163864272+KindGuyJ@users.noreply.github.com>
2024-05-27 08:47:54 -0300

    Case sensitive mayus fixed

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -78,7 +78,7 @@ let PasarPagina = () => {
     let unit,cant, urlComb;
     cant = document.getElementById("distancia").value;
     unit = document.getElementsByName("unidades")[0].value;
-    urlComb = "SegundaWeb.html#" + cant + "#" + unit;
+    urlComb = "segundaWeb.html#" + cant + "#" + unit;
     window.open(urlComb);
 }
 
@@ -97,7 +97,7 @@ let PasarPagina1 = () => {
     cant = document.getElementById("distancia").value;
     sessionStorage.setItem("cantidad",cant);
     sessionStorage.setItem("unidad",unit);
-    window.open("SegundaWeb1.html");
+    window.open("segundaWeb1.html");
 }
 
 let CargaPag1 = () => {
